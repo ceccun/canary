cca86ca5a07d0a159ed0b4e7a812a2d9cde648d8
stel-la <59512425+stel-la@users.noreply.github.com>
2021-01-26 15:45:05 +0000

    yes

--- js/search.js
+++ js/search.js
@@ -1,4 +1,5 @@
 window["query"] = new URL(window.location).searchParams.get("q");
+window["initquery"] = new URL(window.location).searchParams.get("q");
 window["firstItems"] = 0;
 
 flsestrings["wJpGijhQgX"]={
@@ -9,7 +10,15 @@ flsestrings["UTQ1eptGx9"]={
 "default": "<p>Connect your Stella ID account to get Pickup activity from other devices.</p>"
 }
 function flseLoadcall(){
-
+    const uri = document.URL;
+    const currenturi = uri.split("#gsc.tab=")[1].split("&")[0];
+    if(currenturi == "0"){
+        document.getElementById("imgbtn").setAttribute("class","");
+        document.getElementById("txtbtn").setAttribute("class","active");
+    } else{
+        document.getElementById("txtbtn").setAttribute("class","");
+        document.getElementById("imgbtn").setAttribute("class","active");
+    }
 }
 
 function flseUpdate(){
@@ -28,3 +37,14 @@ function flseUpdate(){
     }
         document.getElementById('BG8dt1y1iW').value = window["query"];
 }
+
+document.getElementById("txtbtn").addEventListener("click", ()=>{
+    window.location = 'search.html?q=' + encodeURIComponent(window["initquery"]) + '#gsc.tab=0&gsc.q=' + encodeURIComponent(window["initquery"]) +'&gsc.page=1';
+    document.getElementById("imgbtn").setAttribute("class","");
+    document.getElementById("txtbtn").setAttribute("class","active");
+});
+document.getElementById("imgbtn").addEventListener("click", ()=>{
+    window.location = 'search.html?q=' + encodeURIComponent(window["initquery"]) + '#gsc.tab=1&gsc.q=' + encodeURIComponent(window["initquery"]) +'&gsc.page=1';
+    document.getElementById("txtbtn").setAttribute("class","");
+    document.getElementById("imgbtn").setAttribute("class","active");
+});
