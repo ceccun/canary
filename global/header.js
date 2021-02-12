08295669368d824c76ef18f80a9d4cc7e4e591f8
Ejaz Ali <ejazali4@protonmail.ch>
2021-02-12 10:03:50 +0000

    new changes!!

--- global/header.js
+++
@@ -1,9 +0,0 @@
-fetch('/global/header.html')
-.then((response)=>{
-    if (response.status == 200){
-    response.text().then((data)=>{
-        var newt = document.createElement('div');
-        newt.innerHTML = data;
-        document.getElementById("ha1").appendChild(newt);
-    })
-}});
---
+++ js/sport-index.js
@@ -0,0 +1,18 @@
+flsestrings["vgcAksHPWK"] = {
+    "default":"Recent Activity",
+}
+flsestrings["BbytGVQHeF"] = {
+    "default": "Set up Sport",
+    "ja": "スポーツを設定する"
+}
+flsestrings["XBFkUsnwgA"] = {
+    "default": "Select your preferences and choose your favourite teams to make Sport work best for you.",
+    "ja":"あなたの好みを選択し、あなたのお気に入りのチームを選択して、スポーツをあなたに最適なものにします."
+}
+
+if (navigator.language == "zh"){
+    alert("抱歉! 该服务不适用于您所使用的语言或地区。")
+}
+if (navigator.language == "zh-CN"){
+    window.location.replace("/");
+}
